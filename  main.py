c1ce6377f9f689e6267298090f543100a72dfd31
pkozlows <pkozlows@caltech.edu>
2023-01-24 18:40:11 -0800

    generate_determinants completed

---  main.py
+++  main.py
@@ -1,19 +1,11 @@
-# inputs
-# number of electrons
-electrons=6
-# number of special orbitals
-orbs=6
-# all possible spin orbitals of this system in a list.
-# and odd reflex a spin down orbital and
-# and even reflex a spin up orbital 
-spin_orbs=list()
-for orb in range(1,(orbs * 2)+1):
-     spin_orbs.append(orb)
-
-possible_determinants=list()
-determinant=set()
-for electron in range(electrons):
-    determinant.add(spin_orbs[electron])
-# this is the ground state determinants
-print(determinant)
-     
+import itertools
+elec_in_system=6
+orbs_in_system=6
+def generate_determinants(electrons, orbs):
+   possible_determinants=list() 
+   # all possible spin orbitals of this system in a list.
+   for x in itertools.combinations(range(orbs*2),electrons):
+      possible_determinants.append(set(x))
+   return iter(possible_determinants)
+# asserting whether the ground state determinant is in the object
+assert({*range(elec_in_system)} in generate_determinants(elec_in_system,orbs_in_system))
