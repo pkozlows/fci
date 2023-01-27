cff6b2f9e6d0da36ee6f294bd684b6232effae7c
pkozlows <pkozlows@caltech.edu>
2023-01-26 17:51:29 -0800

    generation of unique pairs

---  main.py
+++  main.py
@@ -1,11 +1,18 @@
 import itertools
+# in puts
 elec_in_system=6
 orbs_in_system=6
-def generate_determinants(electrons, orbs):
-   possible_determinants=list() 
+def gen_unique_pairs(electrons, orbs):
+   """takes in the number of electrons and orbitals and the system.
+   Returns a list of tuples of each unique pair of determinants."""
    # all possible spin orbitals of this system in a list.
-   for x in itertools.combinations(range(orbs*2),electrons):
-      possible_determinants.append(set(x))
-   return iter(possible_determinants)
-# asserting whether the ground state determinant is in the object
-assert({*range(elec_in_system)} in generate_determinants(elec_in_system,orbs_in_system))
+   possible_determinants=list()
+   for determinant in itertools.combinations(range(orbs*2),electrons):
+      possible_determinants.append(set(determinant))
+   # asserting whether the ground state determinant is in the object
+   assert({*range(elec_in_system)} in list(possible_determinants))
+   # create all unique pairs of determinants
+   pairs=itertools.combinations_with_replacement(list(possible_determinants),2)
+   return pairs
+print(compare_determinants(list(gen_unique_pairs(elec_in_system,orbs_in_system))[8]))
+print(list(gen_unique_pairs(elec_in_system,orbs_in_system))[8])
