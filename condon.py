f69fbc67989517a338ea3e6104d59325d49e5ea6
pkozlows <pkozlows@caltech.edu>
2023-02-16 20:04:32 -0800

    ?About spin and2 electron into growls

--- condon.py
+++ condon.py
@@ -92,38 +92,53 @@ def anti_commutator(pair):
 one_elec_ints = np.load("h1e.npy")
 two_elec_ints = np.load("h2e.npy")
 def condon(pair, phase_factor, integrals): 
-    '''takes tuple of two sets that describes how determinants vary,the associated
-      face factor for pudding that determinant pair into maximum coincidence, and a
+    '''takes tuple of two sets with the determinant pair,the associated
+      face (factor for pudding that determinant pair into maximum coincidence, and a
         tuple with the 1e and 2e integrals. returns matrix element'''
+    one_elec_ints = integrals[0]
+    two_elec_ints = integrals[1]
     sq = braket(pair)
     diff = sq.diff()
+    number_of_differences = len(diff[0])
     one_elec_mel = 0
     two_elec_mel = 0
+    # create the relevant matrix for the determined pair
+    # if there is no difference between two determinants
+    if number_of_differences == 0:
+      spin_orbs = set()
+      for operator in sq.combined():
+        spin_orbs.add(operator[0])
+      spacial_indices = [orb // 2 for orb in list(spin_orbs)]
+      one_elec_xgrid = np.ix_(spacial_indices, spacial_indices)  
+      one_elec_mel += np.einsum('ii->', one_elec_ints[one_elec_xgrid])
+      two_elec_xgrid = np.ix_(spacial_indices, spacial_indices, spacial_indices, spacial_indices) 
+      two_elec_mel += (0.5)*(np.einsum('iijj->', two_elec_ints[two_elec_xgrid])-np.einsum('ijji->', two_elec_ints[two_elec_xgrid]))
+      print(two_elec_mel)
+      
+      
+    # if there is one difference between two determinants
+    # if there are two differences bertone to determinants
+    # if len(diff[0])  <= 2:
+    # #   if len(diff[0]) = 1:
+        
+    #   if len(diff[0]) == 0:       
     # store first difference between determinants and convert the spin to spatial index, for later use to access integrals
-    if len(diff[0]) >= 1:
+    if number_of_differences >= 1:
         m = list(diff[0])[0] // 2
         p = list(diff[1])[0] // 2
         # store the 2nd difference
-        if len(diff[0]) >= 2:
+        if number_of_differences >= 2:
             q = list(diff[1])[1] // 2
             n = list(diff[0])[1] // 2
-    # no differences
-    if len(diff[0]) == 0:
-        one_elec_mel += np.einsum('ii->', integrals[0])
-        two_elec_mel += (0.5)*(np.einsum('iijj->', integrals[1])-np.einsum('ijji->', integrals[1]))
     # one difference
-    if len(diff[0]) == 1:
-        one_elec_mel += integrals[0][m,p]
+    if number_of_differences == 1:
+        one_elec_mel += one_elec_ints[m,p]
         # m and p are the orbitals of difference
-        two_elec_mel += np.einsum('ijkk->ij', integrals[1])[m,p]-np.einsum('ijjk->ik', integrals[1])[m,p]
+        two_elec_mel += np.einsum('ijkk->ij', two_elec_ints)[m,p]-np.einsum('ijjk->ik', two_elec_ints)[m,p]
     # 2 differences
-    if len(diff[0]) == 2:
+    #
         # m,p and n,q are orb differences
-        two_elec_mel += integrals[1][m,p,n,q]-integrals[1][m,q,n,p]
-    print(one_elec_mel)
-    print(two_elec_mel)
+        # two_elec_mel += to_election_integrals[m,p,n,q]-[to_election_integrals](one_elec_mel)
+    # print(two_elec_mel)
     return phase_factor*(one_elec_mel + two_elec_mel)
-
-print(condon(({0,1,2,3,4,5}, {0,1,2,3,4,5}), 1, (one_elec_ints, two_elec_ints)))
-
 
