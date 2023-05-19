69bbad562b5e051f4a821118e83a44728fbd3ab0
pkozlows <pkozlows@caltech.edu>
2023-05-19 10:22:23 -0700

    got brill thm right

--- slater.py
+++ slater.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cProfile
+import math
 from pytest import approx
 from anti_commutator import anti_commutator
 
@@ -83,29 +84,38 @@ def condon(pair, integrals):
             return 0
     # if there is one difference, m and p, between the determinants
     if number_of_differences == 1:
-        one_elec_mel += anti_commutator(pair)*kronecker(m_spin, p_spin)*one_elec_special_union_ints[m_special, p_special]
+        one_elec_mel += anti_commutator(pair)*one_elec_ints[m_special, p_special]
+        # print(one_elec_ints)
+        # # print that transpose of the one electron integrals
+        # print(one_elec_ints.T)
         # make custom kronecker function that outputs the fraction from \delta_{[m][n]} or \delta_{[n][p]}
         def kronecker_fraction(m_spin, spin_intersection):
-          """takes a difference spin orbital and a set of common spin orbitals for pair. returns fraction corresponding to 1/sum of delta functions."""
+          """takes a difference spin orbital and a set of common spin orbitals for pair. returns fraction corresponding to relevant sum of delta functions."""
           delta_functions_sum = 0
           for orb in spin_intersection:
               delta_functions_sum += kronecker(m_spin, orb)
-          if delta_functions_sum == 0:
-              return 0
-          else:
-              return 1/delta_functions_sum
-        two_elec_xgrid_coloumb = np.ix_(spacial_union, spacial_union, spacial_intersection, spacial_intersection)
-        two_elec_xgrid_exchange = np.ix_(spacial_union, spacial_intersection, spacial_intersection, spacial_union)
+          return delta_functions_sum/len(spin_intersection)
+        two_elec_xgrid_coloumb = np.ix_(range(6), range(6), spacial_intersection, spacial_intersection)
+        two_elec_xgrid_exchange = np.ix_(range(6), spacial_intersection, spacial_intersection, range(6))
         two_electron_coulomb = two_elec_ints[two_elec_xgrid_coloumb]
         two_electron_exchange = two_elec_ints[two_elec_xgrid_exchange]
-        two_elec_mel += anti_commutator(pair)*kronecker(m_spin, p_spin)*(np.einsum('ijkk->ij', two_electron_coulomb) - np.einsum('ijjk->ik', two_electron_exchange))[m_special, p_special]
+        coulumb_einsum = np.einsum('ijkk->ij', two_electron_coulomb)
+        exchange_einsum = np.einsum('ijjk->ik', two_electron_exchange)
+        # print(coulumb_einsum[2,3])
+        # print(coulumb_einsum[3,2])
+        # print(exchange_einsum[2,3])
+        # print(exchange_einsum[3,2])
+        # print(exchange_einsum)
+        # print(coulumb_einsum)
+        two_elec_mel += anti_commutator(pair)*(np.einsum('ijkk->ij', two_electron_coulomb) - kronecker_fraction(m_spin, spin_intersection)*kronecker_fraction(p_spin, spin_intersection)*np.einsum('ijjk->ik', two_electron_exchange))[m_special,p_special]
     # if there are two differences, m p and n q, between the determinants
     if number_of_differences == 2:
       two_elec_mel += anti_commutator(pair)*(kronecker(m_spin, p_spin)*kronecker(n_spin, q_spin)*two_elec_special_union_ints[m_special,p_special,n_special,q_special] - kronecker(m_spin, q_spin)*kronecker(n_spin, q_spin)*two_elec_special_union_ints[m_special,q_special,n_special,p_special])
     return one_elec_mel + two_elec_mel
 # unit testing
-assert(condon(({0,1,2,3,4,5}, {0,1,2,3,4,5}), (one_elec_ints, two_elec_ints)) == -7.739373948970316)
-assert(condon(({0,1,2,3,4,5}, {0,1,2,3,4,6}), (one_elec_ints, two_elec_ints)) == 0)
+# assert(condon(({0,1,2,3,4,5}, {0,1,2,3,4,5}), (one_elec_ints, two_elec_ints)) == -7.739373948970316)
+# print(condon(({0,1,2,3,4,5}, {0,1,2,3,4,7}), (one_elec_ints, two_elec_ints)))
+assert(math.isclose(condon(({0,1,2,3,4,5}, {0,1,2,3,4,7}), (one_elec_ints, two_elec_ints)), 0, rel_tol=1e-9, abs_tol=1e-12))
 # print(condon(({0,1,2,3,4,5}, {0,1,2,3,4,7}), (one_elec_ints, two_elec_ints)))
 # print(condon(({0,1,2,3,4,5}, {0,1,2,3,5,6}), (one_elec_ints, two_elec_ints)))
 
