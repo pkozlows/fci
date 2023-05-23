b369407f026c19ab5e59b3ec0f0a757b55a32baa
pkozlows <pkozlows@caltech.edu>
2023-05-23 15:21:29 -0700

    removed mentions of the traced object

--- slater.py
+++ slater.py
@@ -2,57 +2,26 @@ import numpy as np
 import cProfile
 import math
 from pytest import approx
-from anti_commutator import anti_commutator
+from face_factor import anti_commutator
 
 # integrals
 one_elec_ints = np.load("h1e.npy")
 two_elec_ints = np.load("h2e.npy")
 
-class braket:
-  """This object takes a   determinant pair.
-    it contains many methods that allow for the determinant pair to be
-      dulled with in second quantization."""
-  def __init__(self, pair):
-    self.pair = pair
-  def diff(self):
-    """returns tuple of  two sets that describe the differences that described the difference between pair.""" 
-    return (self.pair[0].difference(self.pair[1]), self.pair[1].difference(self.pair[0]))
-  # define a ket in second quantizationo
-  def ket(self):
-    """. Converts the orbs of the second determined
-      into a list of appropriate ordered creation operators."""
-    ket_sq = list((orbital, 1) for orbital in self.pair[1])
-    return ket_sq
-  # define a bra in second quantization
-  def bra(self):
-    """Converts the orbs of the first determined into a list of
-     appropriate ordered annihilation operators."""
-    bra_sq = list((orbital, 0) for orbital in self.pair[0])
-    bra_sq.reverse()
-    return bra_sq
-  def diff(self):
-   """returns tuple of  two sets that describe the differences that described the difference between pair.""" 
-   return (self.pair[0].difference(self.pair[1]), self.pair[1].difference(self.pair[0]))
-  def combined(self):
-    """Returns unsimplified list of the creation and angulation ops Form each determinant."""
-    _braket = self.bra() + self.ket()
-    return _braket
-
 def condon(pair, integrals):
     """Takes tuple that is two sets which constitutes a pair of determinants and also a tuple with one and two electron integrals. returns the matrix element Between the pair."""
     one_elec_ints = integrals[0]
     two_elec_ints = integrals[1]
-    sq = braket(pair)
     # get the difference between the two determinants and its length
-    diff = sq.diff()
-    number_of_differences = len(diff[0])
+    differences = (pair[0].difference(pair[1]), pair[1].difference(pair[0]))
+    number_of_differences = len(differences[0])
     # initializes the mels
     one_elec_mel = 0
     two_elec_mel = 0
     # create a set which contains the union of spin orbs in the pair
-    spin_union = sq.pair[0].union(sq.pair[1])
+    spin_union = sorted(pair[0].union(pair[1]))
     # create a set which contains the intersection of spin orbs in the pair
-    spin_intersection = sq.pair[0].intersection(sq.pair[1])
+    spin_intersection = sorted(pair[0].intersection(pair[1]))
     # convert the set of spin orbs to special orbs
     spacial_union = [orb // 2 for orb in spin_union]
     spacial_intersection = [orb // 2 for orb in spin_intersection]
@@ -67,8 +36,8 @@ def condon(pair, integrals):
       two_elec_mel += (1/2)*(np.einsum('iijj->', two_elec_special_union_ints) - (1/2)*np.einsum('ijji->',two_elec_special_union_ints))
     # save the spin orb differences between the determinants and then convert them into special indices for later use the access ints
     if number_of_differences >= 1:
-        first_det_difference = list(diff[0])
-        second_det_difference = list(diff[1])
+        first_det_difference = list(differences[0])
+        second_det_difference = list(differences[1])
         # sort them so that the first difference is the lowest
         first_det_difference.sort()
         second_det_difference.sort()
--- testing_einsum.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-# testing out somethings relating to np.einsum
-integrals = np.load("h2e.npy")
-spelled_gone = list(0,0,1,1,2,2,3)
-spin_intersection = list(0,0,1,1,2)
-
-    
-    
