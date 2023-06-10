22085555c1627e2001fa98b7928f2320a7b26000
Patryk Kozlowski <pkozlows@caltech.edu>
2023-06-10 16:08:51 -0700

    before converting face from pair to difference

--- face_factor.py
+++ face_factor.py
@@ -1,8 +1,36 @@
 def anti_commutator(pair):
-  """takes a pair of determinat sets, and returns the face factor. can only deal with cases where there is one or two differences between determinants"""
-  # make list for what orbs they differ in
-  bra_unique_orbs = sorted(pair[0].difference(pair[1]))
-  ket_unique_orbs = sorted(pair[1].difference(pair[0]))
+  """
+  Takes tuple of a pair of determinants that contains a tuple of two spin strings sets, which are alpha and beta, respectively, and returns the face factor.can only deal with cases where there is one or two differences between determinants.
+    Returns the face factor between the pair.
+    
+    Args:
+        pair (tuple): Tuple of determinants ((alpha, beta), (alpha, beta)).
+                      Each determinant is a tuple of two spin strings.
+    Returns:
+        int: face factor between the pair."""
+  # divine the spin strings for the alpha and the beta orbitals in the bra and ket
+  bra_alpha = sorted(pair[0][0])
+  bra_beta = sorted(pair[0][1])
+  ket_alpha = sorted(pair[1][0])
+  ket_beta = sorted(pair[1][1] )
+  # determine the alpha differences between the two determinants
+  bra_alpha_difference = sorted(bra_alpha.difference(ket_alpha))
+  ket_alpha_difference = sorted(ket_alpha.difference(bra_alpha))
+  assert(len(bra_alpha_difference) == len(ket_alpha_difference))
+  # determine the beta differences between the two determinants
+  bra_beta_difference = sorted(bra_beta.difference(ket_beta))
+  ket_beta_difference = sorted(ket_beta.difference(bra_beta))
+  assert(len(bra_beta_difference) == len(ket_beta_difference))
+  # first treat the case with only one difference of the same spin
+  if len(bra_alpha_difference) == 1 or len(bra_beta_difference) == 1:
+    # determine whether we are dealing with a difference in alpha or beta
+    if len(bra_alpha_difference) == 1:
+      bra_unique_orbs = bra_alpha_difference
+      ket_unique_orbs = ket_alpha_difference
+    elif len(bra_beta_difference) == 1:
+      bra_unique_orbs = bra_beta_difference
+      ket_unique_orbs = ket_beta_difference
+  
   # treat the case with 2 differences of mixed spin
   if len(bra_unique_orbs) == 2 and bra_unique_orbs[0] % 2 != bra_unique_orbs[1] % 2:
     # check whether spins are indeed mixed
